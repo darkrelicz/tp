aea1286a3a28e8f014870e78ce3ca043783d723c
Tan Yu Quan <22824928+yuquanzzz@users.noreply.github.com>
2026-03-21 13:55:24 +0800

    Fix JavaDoc grammar and escape email in PersonListCard.fxml

--- src/main/java/seedu/address/logic/commands/ViewCommand.java
+++ src/main/java/seedu/address/logic/commands/ViewCommand.java
@@ -11,7 +11,7 @@ import seedu.address.model.Model;
 import seedu.address.model.person.Person;
 
 /**
- * Selects a person identified using it's displayed index from the address book to view details.
+ * Selects a person identified using its displayed index from the address book to view details.
  */
 public class ViewCommand extends Command {
 

47e6e20d441faaebb42723430ff7ba72139cb8ae
Tan Yu Quan <22824928+yuquanzzz@users.noreply.github.com>
2026-03-25 17:46:16 +0800

    Rename delete person subcommand to student

--- src/main/java/seedu/address/logic/commands/DeletePersonCommand.java
+++ src/main/java/seedu/address/logic/commands/DeletePersonCommand.java
@@ -12,14 +12,14 @@ import seedu.address.model.person.Person;
  */
 public class DeletePersonCommand extends DeleteCommand {
 
-    public static final String SUB_COMMAND_WORD = "person";
+    public static final String SUB_COMMAND_WORD = "student";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + " " + SUB_COMMAND_WORD
-            + ": Deletes the person identified by the index number used in the displayed person list.\n"
-            + "Parameters: person INDEX (must be a positive integer)\n"
+        + ": Deletes the student identified by the index number used in the displayed student list.\n"
+        + "Parameters: INDEX (must be a positive integer)\n"
             + "Example: " + COMMAND_WORD + " " + SUB_COMMAND_WORD + " 1";
 
-    public static final String MESSAGE_DELETE_PERSON_SUCCESS = "Deleted Person: %1$s";
+    public static final String MESSAGE_DELETE_PERSON_SUCCESS = "Deleted Student: %1$s";
 
     public DeletePersonCommand(Index index) {
         super(index);

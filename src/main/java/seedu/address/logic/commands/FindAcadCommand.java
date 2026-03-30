1fa7446c7ff2ffaf43eb60d06cf8aaa434aa68c4
vmd <vuminhduc.vmd.vn@gmail.com>
2026-03-30 23:14:43 +0800

    Fix find tag and find acad chaining bugs

--- src/main/java/seedu/address/logic/commands/FindAcadCommand.java
+++ src/main/java/seedu/address/logic/commands/FindAcadCommand.java
@@ -41,7 +41,7 @@ public class FindAcadCommand extends FindCommand {
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
-        model.updateFilteredPersonList(predicate);
+        model.updateFilteredPersonListWithAnd(predicate);
         model.setListDisplayMode(ListDisplayMode.PERSON);
         return new CommandResult(
                 String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW,

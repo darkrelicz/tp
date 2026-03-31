242cc9f1d0e69517e9cd676a3dca27afab5c6a2c
vmd <vuminhduc.vmd.vn@gmail.com>
2026-03-31 13:28:50 +0800

    Add support for non-destructive tag addition

---
+++ src/main/java/seedu/address/logic/commands/AddTagCommand.java
@@ -0,0 +1,97 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.logic.Messages;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.PersonBuilder;
+import seedu.address.model.tag.Tag;
+
+/**
+ * Adds tag(s) to an existing person in the address book.
+ */
+public class AddTagCommand extends AddCommand {
+
+    public static final String SUB_COMMAND_WORD = "tag";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + " " + SUB_COMMAND_WORD
+            + ": Adds tag(s) to the student identified by the index number used in the displayed student list. "
+            + "Existing tags will be kept, and new tags will be added.\n"
+            + "Parameters: INDEX (must be a positive integer) "
+            + PREFIX_TAG + "TAG...\n"
+            + "Example: " + COMMAND_WORD + " " + SUB_COMMAND_WORD + " 1 "
+            + PREFIX_TAG + "friends " + PREFIX_TAG + "CS2103";
+
+    public static final String MESSAGE_ADD_TAG_SUCCESS = "Added Tags to Person: %1$s";
+
+    private final Index index;
+    private final Set<Tag> tagsToAdd;
+
+    /**
+     * @param index of the person in the filtered person list to edit
+     * @param tagsToAdd tags to be added to the person's existing tags
+     */
+    public AddTagCommand(Index index, Set<Tag> tagsToAdd) {
+        requireNonNull(index);
+        requireNonNull(tagsToAdd);
+
+        this.index = index;
+        this.tagsToAdd = new HashSet<>(tagsToAdd);
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+
+        if (index.getZeroBased() >= model.getFilteredPersonList().size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        }
+
+        Person personToEdit = model.getFilteredPersonList().get(index.getZeroBased());
+
+        Set<Tag> updatedTags = new HashSet<>(personToEdit.getTags());
+        updatedTags.addAll(tagsToAdd);
+
+        Person editedPerson = new PersonBuilder(personToEdit)
+                .withTags(updatedTags)
+                .build();
+
+        model.setPerson(personToEdit, editedPerson);
+        model.updateFilteredPersonList(Model.PREDICATE_SHOW_ALL_PERSONS);
+
+        return new CommandResult(
+                String.format(MESSAGE_ADD_TAG_SUCCESS, Messages.format(editedPerson)),
+                editedPerson);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof AddTagCommand)) {
+            return false;
+        }
+
+        AddTagCommand otherCommand = (AddTagCommand) other;
+        return index.equals(otherCommand.index)
+                && tagsToAdd.equals(otherCommand.tagsToAdd);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("index", index)
+                .add("tagsToAdd", tagsToAdd)
+                .toString();
+    }
+}

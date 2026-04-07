6ea2c4567e643ae45c31e0972e0dfd033a91296f
beedahju <judahpang@gmail.com>
2026-04-07 11:01:25 +0800

    Fix checkstyle issues

--- src/test/java/seedu/address/logic/parser/EditPersonCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/EditPersonCommandParserTest.java
@@ -1,8 +1,9 @@
 package seedu.address.logic.parser;
+
 import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_AMY;
-import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 
